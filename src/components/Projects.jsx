74fc9bfddb757d206e825e4686c70d8cf5ec9ea0
Kaspars <smarnss@gmail.com>
2025-02-27 19:12:30 +0200

    Initialize page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div
+      className="flex min-h-screen w-full flex-col items-center justify-center gap-20 p-10 md:p-16 xl:p-32"
+      id="projects"
+    >
+      <h1 className="text-center text-5xl font-light">Projects</h1>
+
+      <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+            asdasd
+          </p>
+        </div>
+
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+           asdasd
+          </p>
+        </div>
+
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+            asdasd
+          </p>
+        </div>
+
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+            asdasd
+          </p>
+        </div>
+
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+            asdasd
+          </p>
+        </div>
+
+        <div className="cursor-pointer space-y-2 rounded-lg border-2 border-teal-500 px-8 py-10 text-left transition-all duration-200 hover:scale-105 hover:bg-teal-50">
+          <h1 className="text-3xl font-semibold">Project</h1>
+          <h3 className="text-xl text-gray-700">Tech Stack</h3>
+          <p className="text-sm text-gray-600">
+            asdasd
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
